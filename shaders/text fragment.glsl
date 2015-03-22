6ab1d79e829a3636454965a28a4488d8fbafb324
James Brown <jamesphone180@gmail.com>
2015-03-22 14:51:05 +1300

    Glsl compatability changes
    fixed the glsl code so it works on AMD GPUs

--- shaders/text fragment.glsl
+++ shaders/text fragment.glsl
@@ -1,12 +1,12 @@
 #version 430 core
 
-layout(binding = 0, location = 1) uniform sampler2D texture;
+layout(binding = 0, location = 1) uniform sampler2D text;
 
 layout(location = 0) out vec4 colour;
 
 layout(location = 0) in vec2 uv;
 
 void main() {
-	colour.a = texture(texture, uv).r;
+	colour.a = texture(text, uv).r;
 	colour.rgb = vec3(1.0, 1.0, 1.0);
 }
